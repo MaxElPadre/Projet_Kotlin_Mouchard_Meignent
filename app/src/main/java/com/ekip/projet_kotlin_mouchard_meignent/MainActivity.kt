a15f779a67c18fa9189fbb99503381a071606083
MaxElPadre <clairson.max@gmail.com>
2021-05-19 17:27:18 +0200

    init repository

---
+++ app/src/main/java/com/ekip/projet_kotlin_mouchard_meignent/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ekip.projet_kotlin_mouchard_meignent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/ekip/projet_kotlin_mouchard_meignent/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.ekip.projet_kotlin_mouchard_meignent
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
