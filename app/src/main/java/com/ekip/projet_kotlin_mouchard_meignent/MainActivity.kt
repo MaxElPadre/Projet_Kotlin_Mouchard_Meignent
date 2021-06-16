4775baaf74b05d0739ef2eef3cf929930609af4c
Max <clairson.max@gmail.com>
2021-06-17 00:33:07 +0200

    Feat: CustomException, Enum & Extensions

--- app/src/main/java/com/ekip/projet_kotlin_mouchard_meignent/MainActivity.kt
+++ app/src/main/java/com/ekip/projet_kotlin_mouchard_meignent/MainActivity.kt
@@ -9,3 +9,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+const val excellent = "Excellent score !"
+const val great =  "Bon score !"
+const val medium = "Tu peux mieux faire"
+const val bad = "Franchement pas ouf ..."
+
+enum class Scoring { EXCELLENT, GREAT, MEDIUM, BAD }
+
+class ExtensionMethods {
+    fun Scoring.displayScoreResult(): String = when(this) {
+        Scoring.EXCELLENT -> excellent
+        Scoring.GREAT -> great
+        Scoring.MEDIUM -> medium
+        Scoring.BAD -> bad
+        else -> throw BadEnumException()
+    }
+
+    fun Int.displayScoreResult(totalQuestion: Int): String = "Ton score est de $this/$totalQuestion"
+}
+
+class BadEnumException(val customMessage: String = "Wrong enumerator value") : Exception()
